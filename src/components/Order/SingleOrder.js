6db8ce92eed2ed8cfbe387e51da6e214b99d9eb6
arhamathar <arhamathar789@gmail.com>
2020-12-21 00:28:26 +0530

    Fetching and displaying the orders

--- src/components/Order/SingleOrder.js
+++ src/components/Order/SingleOrder.js
@@ -2,21 +2,53 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-    width: 100%;
+    width:70%;
     border: 1px solid #eee;
-    box-shadow: 0 2px 4px #ccc;
+    box-shadow: 0 3px 5px #ccc;
     padding: 1rem;
-    margin: 1rem;
+    margin: 1rem auto;
     box-sizing: border-box;
     font-size: 1.25rem;
+    line-height: 1.25;
+    @media(max-width: 1013px){
+        width: 80%
+    }
+    @media(max-width: 555px){
+        width: 85%
+    }
 `;
 
-function SingleOrder() {
+const StyledSpan = styled.span`
+    text-transform: capitalize;
+    display: inline-block;
+    border: 1px solid #ccc;
+    padding: .5rem;
+    margin: 0.8rem;
+
+`;
+
+function SingleOrder(props) {
+    const ingredients = [];
+    // conveting object of ingredients in an array short method than burger component.
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
     return (
         <StyledDiv>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong> ₹ 100</strong></p>
-        </StyledDiv>
+            <p>
+                Ingredients: <br />
+                {ingredients.map(ingredient => {
+                    return <StyledSpan key={ingredient.name}>
+                        {ingredient.name} <strong>({ingredient.amount})</strong>
+                    </StyledSpan>
+                })}
+            </p>
+            <p>Price: <strong> ₹ {props.price}</strong></p>
+        </StyledDiv >
     );
 }
 
--- src/containers/MyOrders.js
+++ src/containers/MyOrders.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SingleOrder from '../components/Order/SingleOrder';
+import ErrorModal from '../components/UI/Error/ErrorModal';
+import axios from '../axios-orders';
 
 
 function MyOrders() {
+    const [error, setError] = useState(false);
+    const [orders, setOrders] = useState([]);
+    const [isloading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const sendRequest = async () => {
+            try {
+                const response = await axios.get('/orders.json');
+                console.log(response.data);
+                let fetchedOrder = [];
+                for (let key in response.data) {
+                    fetchedOrder.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                setOrders(fetchedOrder);
+                setIsLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setIsLoading(false);
+            }
+        }
+        sendRequest();
+    }, []);
+
+    const clearErrorHandler = () => {
+        setError(null);
+    }
+
     return (
-        <div>
-            <SingleOrder />
-        </div>
+        <React.Fragment>
+            <ErrorModal showError={error} onClear={clearErrorHandler} />
+            {orders.map(order => {
+                return <SingleOrder
+                    key={order.id}
+                    price={order.price}
+                    ingredients={order.ingredients}
+                />
+            })}
+        </React.Fragment>
     );
 }
 
