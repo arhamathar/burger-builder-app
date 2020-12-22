a5a61f144a576017e12645487e3edd74d3a78716
arhamathar <arhamathar789@gmail.com>
2020-12-22 21:53:59 +0530

    Form submission added

--- src/containers/ContactData.js
+++ src/containers/ContactData.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from '../axios-orders';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
+import Spinner from '../components/UI/Spinner/Spinner';
+import ErrorModal from '../components/UI/Error/ErrorModal';
 import Input from '../components/FormElements/Input/Input';
 import Button from '../components/FormElements/Button/Button';
 
@@ -16,8 +21,10 @@ const StyledDiv = styled.div`
     }
 `;
 
-function ContactData() {
-    // const [isLoading, setIsLoading] = useState(false);
+function ContactData(props) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
     const [name, setName] = useState({
         value: '',
         isValid: false
@@ -45,32 +52,32 @@ function ContactData() {
         { value: 'cheapest', displayValue: 'Cheapest' }
     ]
 
-    const orderHandler = (e) => {
+    const history = useHistory();
+
+    const orderHandler = async (e) => {
         e.preventDefault();
-        // setIsLoading(true);
-        // const order = {
-        //     ingredients: ingredients,
-        //     price: totalPrice,
-        //     customer: {
-        //         name: "Arham Athar",
-        //         email: "arham@gmail.com",
-        //         address: {
-        //             street: 'Pateri',
-        //             pinCode: 485001,
-        //             state: 'M.P'
-        //         }
-        //     },
-        //     deliveryMethod: 'express'
-        // }
-        // try {
-        //     await axios.post('/orders.json', order);
-        //     setIsLoading(false);
-        //     setShowModal(false);
-        // } catch (err) {
-        //     setError(err.message);
-        //     setIsLoading(false);
-        //     setShowModal(false);
-        // }
+        setIsLoading(true);
+        const order = {
+            ingredients: props.ings,
+            price: props.price,
+            customer: {
+                name: name,
+                email: email,
+                address: {
+                    street: street,
+                    pinCode: pincode
+                }
+            },
+            deliveryMethod: delivery
+        }
+        try {
+            await axios.post('/orders.json', order);
+            setIsLoading(false);
+            history.push("/");
+        } catch (err) {
+            setError(err.message);
+            setIsLoading(false);
+        }
     }
 
     const inputChangeHandler = (e, inputId) => {
@@ -120,57 +127,72 @@ function ContactData() {
         }
     }
 
+    const clearError = () => {
+        setError(null);
+    }
+
     return (
-        <StyledDiv>
-            <h4>Enter your Contact Data</h4>
-            <form onSubmit={orderHandler}>
-                <Input
-                    inputtype="input"
-                    id="name"
-                    type="text"
-                    name="name"
-                    placeholder="Your Name"
-                    onChange={inputChangeHandler}
-                    value={name.value}
-                />
-                <Input
-                    inputtype="input"
-                    id="email"
-                    type="text"
-                    name="email"
-                    placeholder="Your Email"
-                    onChange={inputChangeHandler}
-                    value={email.value}
-                />
-                <Input
-                    inputtype="input"
-                    id="street"
-                    type="text"
-                    name="street"
-                    placeholder="Street"
-                    onChange={inputChangeHandler}
-                    value={street.value}
-                />
-                <Input
-                    inputtype="input"
-                    id="pincode"
-                    type="text"
-                    name="pincode"
-                    placeholder="Pin Code"
-                    onChange={inputChangeHandler}
-                    value={pincode.value}
-                />
-                <Input
-                    inputtype="select"
-                    id="delivery"
-                    options={options}
-                    onChange={inputChangeHandler}
-                    value={delivery.value}
-                />
-                <Button btnType="Success">Order</Button>
-            </form>
-        </StyledDiv>
+        <React.Fragment>
+            <ErrorModal showError={error} onClear={clearError} />
+            <StyledDiv>
+                {isLoading && <Spinner show={isLoading} />}
+                {!isLoading && <h4>Enter your Contact Data</h4>}
+                {!isLoading && <form onSubmit={orderHandler}>
+                    <Input
+                        inputtype="input"
+                        id="name"
+                        type="text"
+                        name="name"
+                        placeholder="Your Name"
+                        onChange={inputChangeHandler}
+                        value={name.value}
+                    />
+                    <Input
+                        inputtype="input"
+                        id="email"
+                        type="text"
+                        name="email"
+                        placeholder="Your Email"
+                        onChange={inputChangeHandler}
+                        value={email.value}
+                    />
+                    <Input
+                        inputtype="input"
+                        id="street"
+                        type="text"
+                        name="street"
+                        placeholder="Street"
+                        onChange={inputChangeHandler}
+                        value={street.value}
+                    />
+                    <Input
+                        inputtype="input"
+                        id="pincode"
+                        type="text"
+                        name="pincode"
+                        placeholder="Pin Code"
+                        onChange={inputChangeHandler}
+                        value={pincode.value}
+                    />
+                    <Input
+                        inputtype="select"
+                        id="delivery"
+                        options={options}
+                        onChange={inputChangeHandler}
+                        value={delivery.value}
+                    />
+                    <Button btnType="Success">Order</Button>
+                </form>}
+            </StyledDiv>
+        </React.Fragment>
     );
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    };
+}
+
+export default connect(mapStateToProps)(ContactData);
