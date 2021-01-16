2b1840792a40defbd6f2cb49c7a4c126fd9ca1f4
arhamathar <arhamathar789@gmail.com>
2021-01-16 19:38:09 +0530

    authContext setup for global state management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Checkout from './containers/Checkout';
@@ -7,33 +7,48 @@ import MyOrders from './containers/MyOrders';
 import ContactData from './containers/ContactData';
 import LogIn from './containers/Auth/Login';
 import SignUp from './containers/Auth/Signup';
+import { AuthContext } from './context/authContext';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    const login = () => {
+        setIsLoggedIn(true);
+    }
+
+    const logout = () => {
+        setIsLoggedIn(false);
+    }
+
     return (
-        <Router>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" >
-                        <BurgerBuilder />
-                    </Route>
-                    <Route path="/checkout">
-                        <Checkout />
-                    </Route>
-                    <Route path="/orders">
-                        <MyOrders />
-                    </Route>
-                    <Route path='/contact-data'>
-                        <ContactData />
-                    </Route>
-                    <Route path='/auth/login'>
-                        <LogIn />
-                    </Route>
-                    <Route path='/auth/signup'>
-                        <SignUp />
-                    </Route>
-                </Switch>
-            </Layout>
-        </Router>
+        <AuthContext.Provider
+            value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+        >
+            <Router>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" >
+                            <BurgerBuilder />
+                        </Route>
+                        <Route path="/checkout">
+                            <Checkout />
+                        </Route>
+                        <Route path="/orders">
+                            <MyOrders />
+                        </Route>
+                        <Route path='/contact-data'>
+                            <ContactData />
+                        </Route>
+                        <Route path='/auth/login'>
+                            <LogIn />
+                        </Route>
+                        <Route path='/auth/signup'>
+                            <SignUp />
+                        </Route>
+                    </Switch>
+                </Layout>
+            </Router>
+        </AuthContext.Provider>
     );
 }
 
